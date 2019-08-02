028081e7d6d2351736a186f29b2eb4e2fdd94b2d
Lambda_School_Loaner_155 <lambda_school_loaner_155@Lambda-School-Loaner-155s-MacBook-Pro.local>
2019-08-02 08:49:19 -0700

    user can now create new todos

--- todo-mvc/src/App.js
+++ todo-mvc/src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Yet another Todo App</h1>
-      
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    todos: [],
+    input: ''
+  }
+
+  handleSubmit = (e) => {
+    // prevent form from reloading
+    e.preventDefault()
+    // add new todo to state
+    this.setState({ todos: this.state.todos.concat(this.state.input) })
+    // reset input field
+    this.setState({input: ''})
+  }
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
+  render() {
+     return (
+      <div className="App">
+        <h1>Yet another Todo App</h1>
+          <ul>
+            {this.state.todos.map(function renderTodos(todo, i) {
+              return <li key={i} className="todo-item">{todo}</li>
+            })}
+          </ul>
+          <form onSubmit={this.handleSubmit}>
+            <input 
+            type="text"
+            placeholder="what needs to get done?"
+            autoFocus
+            value={this.state.input}
+            onChange={this.handleChange}
+          >
+          </input>
+        </form>
+      </div>
+     )  
+  }
+
 }
 
 export default App;
