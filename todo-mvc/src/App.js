e2375d649542da4b21813340a397f751b30384e7
Lambda_School_Loaner_155 <lambda_school_loaner_155@Lambda-School-Loaner-155s-MacBook-Pro.local>
2019-08-01 20:17:13 -0700

    init

---
+++ todo-mvc/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yet another Todo App</h1>
+      
+    </div>
+  );
+}
+
+export default App;
