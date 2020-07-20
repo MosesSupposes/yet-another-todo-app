cea9be7d9ccf65d309df67b99b62ff66a71491de
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-19 17:17:22 -0700

    test commit

--- todo-mvc/src/App.js
+++ todo-mvc/src/App.js
@@ -1,48 +1,49 @@
-import React from 'react';
-
+import React from "react";
 
 class App extends React.Component {
-  state = {
-    todos: [],
-    input: ''
-  }
+	state = {
+		todos: [],
+		input: "",
+	};
 
-  handleSubmit = (e) => {
-    // prevent page from reloading
-    e.preventDefault()
-    // add new todo to state
-    this.setState({ todos: this.state.todos.concat(this.state.input) })
-    // reset input field
-    this.setState({input: ''})
-  }
+	handleSubmit = e => {
+		// prevent page from reloading
+		e.preventDefault();
+		// add new todo to state
+		this.setState({ todos: this.state.todos.concat(this.state.input) });
+		// reset input field
+		this.setState({ input: "" });
+	};
 
-  handleChange = (e) => {
-    this.setState({ input: e.target.value })
-  }
+	handleChange = e => {
+		this.setState({ input: e.target.value });
+	};
 
-  render() {
-     return (
-      <div className="App">
-        <h1>Yet another Todo App</h1>
-          <ul>
-            {this.state.todos.map(function renderTodos(todo, i) {
-              return <li key={i} className="todo-item">{todo}</li>
-            })}
-          </ul>
-          <form onSubmit={this.handleSubmit}>
-            <input 
-            type="text"
-            placeholder="what needs to get done?"
-            autoFocus
-            value={this.state.input}
-            onChange={this.handleChange}
-          >
-          </input>
-        </form>
-      </div>
-     )  
-  }
-
+	render() {
+		return (
+			<div className="App">
+				<h1>Yet another Todo App</h1>
+				<ul>
+					{this.state.todos.map(function renderTodos(todo, i) {
+						return (
+							<li key={i} className="todo-item">
+								{todo}
+							</li>
+						);
+					})}
+				</ul>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="what needs to get done?"
+						autoFocus
+						value={this.state.input}
+						onChange={this.handleChange}
+					></input>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
